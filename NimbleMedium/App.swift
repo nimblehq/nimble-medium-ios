e8f126f75127e51dbd555bf7ac7d485756240f7d
MarkG <markgravity.in@gmail.com>
2021-08-06 10:09:15 +0700

    [#1] Setup project
    - Add necessary 3rd party libraries through SPM
    - Setup Swiftlint
    - Update README

--- NimbleMedium/App.swift
+++ NimbleMedium/App.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import AlamofireNetworkActivityLogger
 
 @main
 struct App: SwiftUI.App {
+    init() {
+        #if DEBUG
+        // Print alamofire request & response log
+        NetworkActivityLogger.shared.level = .debug
+        NetworkActivityLogger.shared.startLogging()
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             HomeView()
