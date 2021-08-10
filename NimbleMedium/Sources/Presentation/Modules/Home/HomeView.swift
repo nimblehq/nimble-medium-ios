db214a1f9719e8f6ec5d5cd51adf1a554bfef71d
MarkG <markgravity.in@gmail.com>
2021-08-10 14:23:44 +0700

    [#7] Create Navigation Bar
    - Add navigation bar modifier
    - Add Screen view

--- NimbleMedium/Sources/Presentation/Modules/Home/HomeView.swift
+++ NimbleMedium/Sources/Presentation/Modules/Home/HomeView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+
     var body: some View {
-        Text(Localizable.homeTitle())
-            .padding()
+        Screen(title: Localizable.homeTitle()) {
+            Text("This is home")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
+    
     static var previews: some View {
         HomeView()
     }
---
+++ NimbleMedium/Sources/Presentation/Views/Screen.swift
@@ -0,0 +1,38 @@
+//
+//  Screen.swift
+//  NimbleMedium
+//
+//  Created by Mark G on 10/08/2021.
+//
+
+import SwiftUI
+
+struct Screen<Content>: View where Content: View {
+
+    private let content: Content
+    private let title: String?
+    private let titleColor: Color
+
+    // swiftlint:disable type_contents_order
+    init(title: String? = nil,
+         titleColor: Color = .white,
+         @ViewBuilder content: () -> Content) {
+        self.content = content()
+        self.title = title
+        self.titleColor = titleColor
+    }
+
+    var body: some View {
+        NavigationView {
+            content
+                .navigationBarTitle(
+                    title ?? "",
+                    displayMode: .inline
+                )
+                .navigationBarColor(
+                    backgroundColor: .green,
+                    titleColor: titleColor
+                )
+        }
+    }
+}
