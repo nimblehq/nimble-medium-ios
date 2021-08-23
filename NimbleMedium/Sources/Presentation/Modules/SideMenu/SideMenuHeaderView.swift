118274dd452af127f6509337eb9bb0eae0d8e8c3
Minh Pham <minh@nimblehq.co>
2021-08-23 16:34:52 +0700

    [#11] Implement left menu header section UI layout when not logged in

---
+++ NimbleMedium/Sources/Presentation/Modules/SideMenu/SideMenuHeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  SideMenuHeaderView.swift
+//  NimbleMedium
+//
+//  Created by Minh Pham on 23/08/2021.
+//
+
+import SwiftUI
+
+struct SideMenuHeaderView: View {
+
+    var body: some View {
+
+        ZStack(alignment: .center) {
+            Color.green.edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center) {
+                Text(Localizable.menuHeaderTitle())
+                    .foregroundColor(.white)
+                    .font(.system(size: 28, weight: .heavy, design: .default))
+                    .padding()
+                Text(Localizable.menuHeaderDescription())
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct SideMenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuHeaderView()
+    }
+}
+#endif
