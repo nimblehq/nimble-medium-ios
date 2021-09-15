9b2e8dbd45b0d4a3c13848b6c10d18bc03b93a97
MarkG <markgravity.in@gmail.com>
2021-09-15 14:42:30 +0700

    [#29] Create Comments Screen UI
    # Conflicts:
    #	NimbleMedium.xcodeproj/project.pbxproj
    #	NimbleMedium/Resources/en.lproj/Localizable.strings
    
    # Conflicts:
    #	NimbleMedium/Resources/en.lproj/Localizable.strings

---
+++ NimbleMedium/Sources/Presentation/Modules/FeedComments/FeedCommentsView.swift
@@ -0,0 +1,37 @@
+//
+//  FeedCommentsView.swift
+//  NimbleMedium
+//
+//  Created by Mark G on 15/09/2021.
+//
+
+import SwiftUI
+
+struct FeedCommentsView: View {
+
+    var body: some View {
+        Content(view: self)
+    }
+}
+
+// MARK: - Content
+private extension FeedCommentsView {
+
+    struct Content: View {
+
+        let view: FeedCommentsView
+
+        var body: some View {
+            ScrollView(.vertical) {
+                LazyVStack(alignment: .leading, spacing: 12.0) {
+                    ForEach(1...3, id: \.self) { _ in
+                        FeedCommentRow()
+                    }
+                }
+                .padding(.all, 16.0)
+            }
+            .navigationTitle(Localizable.feedCommentsTitle())
+            .modifier(NavigationBarPrimaryStyle())
+        }
+    }
+}
