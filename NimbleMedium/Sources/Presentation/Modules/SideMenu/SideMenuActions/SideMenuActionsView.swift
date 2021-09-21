3a3509716d5ba4c7d872e09c2f344b053e1d51f2
Minh Pham <minh@nimblehq.co>
2021-09-21 16:10:14 +0700

    [#40] Implement my profile option for the side menu in authenticated state

--- NimbleMedium/Sources/Presentation/Modules/SideMenu/SideMenuActions/SideMenuActionsView.swift
+++ NimbleMedium/Sources/Presentation/Modules/SideMenu/SideMenuActions/SideMenuActionsView.swift
@@ -13,32 +13,50 @@ struct SideMenuActionsView: View {
 
     @ObservedViewModel private var viewModel: SideMenuActionsViewModelProtocol = Resolver.resolve()
 
-    @State private var isAuthenticated = false
+    // TODO: Update with correct value for isAuthenticated in integrate task
+    @State private var isAuthenticated = true
     @State private var isShowingLoginScreen = false
     @State private var isShowingSignupScreen = false
 
     var body: some View {
-        VStack(alignment: .leading) {
-            if !isAuthenticated {
-                SideMenuActionItemView(
-                    text: Localizable.menuOptionLogin(),
-                    iconName: R.image.iconLogin.name
-                ) {
-                    viewModel.input.selectLoginOption()
-                }
-                .fullScreenCover(isPresented: $isShowingLoginScreen) {
-                    LoginView()
-                }
+        if isAuthenticated {
+            authenticatedMenuOptions
+        } else {
+            unauthenticatedMenuHeader
+        }
+    }
 
-                SideMenuActionItemView(
-                    text: Localizable.menuOptionSignup(),
-                    iconName: R.image.iconSignup.name
-                ) {
-                    viewModel.input.selectSignupOption()
-                }
-                .fullScreenCover(isPresented: $isShowingSignupScreen) {
-                    SignupView()
-                }
+    var authenticatedMenuOptions: some View {
+        VStack(alignment: .leading) {
+            SideMenuActionItemView(
+                text: Localizable.menuOptionMyProfile(),
+                iconName: R.image.iconMyProfile.name
+            ) {
+                // TODO: Implement this option tap action in the integrate task
+            }
+        }
+    }
+
+    var unauthenticatedMenuHeader: some View {
+        VStack(alignment: .leading) {
+            SideMenuActionItemView(
+                text: Localizable.menuOptionLogin(),
+                iconName: R.image.iconLogin.name
+            ) {
+                viewModel.input.selectLoginOption()
+            }
+            .fullScreenCover(isPresented: $isShowingLoginScreen) {
+                LoginView()
+            }
+
+            SideMenuActionItemView(
+                text: Localizable.menuOptionSignup(),
+                iconName: R.image.iconSignup.name
+            ) {
+                viewModel.input.selectSignupOption()
+            }
+            .fullScreenCover(isPresented: $isShowingSignupScreen) {
+                SignupView()
             }
         }
         .onReceive(viewModel.output.didSelectLoginOption) {
