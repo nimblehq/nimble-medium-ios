05d93bd24cdadea95195d7b60497cc38c647b9f3
Minh Pham <minh@nimblehq.co>
2021-10-04 17:07:54 +0700

    [chore] Force using `https://realworld-temp-api.herokuapp.com/api` endpoint for stability purpose.

--- NimbleMedium/Sources/Constants/Constants+API.swift
+++ NimbleMedium/Sources/Constants/Constants+API.swift
@@ -9,5 +9,5 @@
 extension Constants.API {
 
     // For fixing performance issues and the presence of NSFW content on the current API, we encourage you to use the new temporary API: https://realworld-temp-api.herokuapp.com/api Please keep in mind we'll soon move back to the current API: https://conduit.productionready.io/api
-    static let baseURL: String = "https://conduit.productionready.io/api"
+    static let baseURL: String = "https://realworld-temp-api.herokuapp.com/api"
 }
